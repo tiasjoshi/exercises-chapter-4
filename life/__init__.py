ad5eb8bee0bffd6e129c4846e7d120707854be2c
Tia Joshi <149258608+tiasjoshi@users.noreply.github.com>
2026-04-18 10:17:15 +0100

    chapter 4 flake8 pass check

--- life/__init__.py
+++ life/__init__.py
@@ -1,1 +1,1 @@
-from .life import glider, blinker, glider_gun, Game
+from .life import glider, blinker, glider_gun, Game # noqa F401
--- life/life.py
+++ life/life.py
@@ -1,14 +1,15 @@
-import numpy as np
+import numpy as np # noqa F401
+
+
 from matplotlib import pyplot
 from scipy.signal import convolve2d
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
+glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
 
 blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]]
-    )
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0]])
 
 glider_gun = np.array([
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -49,30 +50,33 @@ glider_gun = np.array([
     [0, 0, 1, 1, 0, 0, 0, 0, 0]
 ])
 
-class Game:
-    def __init__(game,Size):
+class Game: # noqa F401
+    def __init__(game, Size): # noqa F401
         game.board = np.zeros((Size, Size))
-    
-    def play(self):
+
+    def play(self): # noqa F401
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move()
+            self.show()
             pyplot.pause(0.0000005)
 
-    def move(self):
-        STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-        NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
+    def move(self): # noqa F401
+        stencil = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+        neighbourcount = convolve2d(self.board, stencil, mode='same')
 
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                self.board[i, j] = 1 if (neighbourcount[i, j] == 3
+                                         or (neighbourcount[i, j] == 2
+                                             and self.board[i, j])) else 0
 
-    def __setitem__(self, key, value): 
+    def __setitem__(self, key, value): # noqa F401
         self.board[key] = value
 
 
-    def show(self):
-        pyplot.clf() 
+    def show(self): # noqa F401
+        pyplot.clf()
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
